4008dabbb30da54f8fc85395574ef4c89f7c340d
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-10-13 18:11:53 -0300

    Debug

--- src/services/mysql/auth.js
+++ src/services/mysql/auth.js
@@ -11,7 +11,6 @@ const auth = deps => {
 
         connection.query(queryString, queryData, (error, results) => {
           if (error || !results.length) {
-            console.log(error)
             errorHandler(error, 'Falha ao localizar o usuário', reject)
             return false
           }
